9bd4c947d8448def346a62ec60ccb1011eb78f3a
ddmitov <ddmitov@yahoo.com>
2018-10-14 14:39:12 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Magna Carta
+--------------------------------------------------------------------------------
+
+Magna Carta е Perl 5 Mojolicious::Lite уеб приложение за платформата Heroku.  
+То представя текста на Magna Carta Libertatum от 1215 г. заедно с честотен индекс на всички словоформи.  
+
+Magna Carta is a Perl 5 Mojolicious::Lite web application for the Heroku platform.  
+It presents the text of Magna Carta Libertatum of 1215 with a word frequency index.  
+
+## [Credits](./CREDITS.md)
+
+## [License](./LICENSE.md)
+Magna Carta is licensed under the Creative Commons Attribution-ShareAlike License.  
+Dimitar D. Mitov 2018  
---
+++ magna-carta-index-maker.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my %word_count;
+
+# Open the input file:
+open my $input_filehandle, '<', "magna-carta.txt" or
+  die "Could not open magna-carta.txt!";
+my @text = <$input_filehandle>;
+close $input_filehandle;
+
+# Extract all word forms in the 'word_count' hash:
+foreach my $line (@text) {
+  chomp $line;
+  $line =~ s/\d{1,}|\,|:|;|\.//g;
+  foreach my $word (split /\s+/, $line) {
+    if ($word =~ /^\w{1,}$/) {
+      $word_count{$word}++;
+    }
+  }
+}
+
+# Output is a ready to use HTML code for a vertical scroll menu
+# calling a JavaScript function to highlight clicked words:
+
+open my $output_filehandle, '>', "magna-carta-index.txt" or
+  die "Could not open magna-carta-index.txt!";
+
+# Sort the results alphabetically:
+foreach my $word (sort {lc($a) cmp lc($b)} keys %word_count) {
+  my $formatted_word_count = sprintf("%03d", $word_count{$word});
+  print $output_filehandle
+    "<a href=\"javascript:highlight('$word');\">$formatted_word_count&nbsp;&nbsp;$word</a>\n"
+}
+
+print $output_filehandle "\n==============================\n\n";
+
+# Sort the results according to frequency,
+# words with equal frequency are sorted alphabetically:
+foreach my $word (reverse sort {$word_count{$a} <=> $word_count{$b} ||
+                                lc($b) cmp lc($a)}
+                                keys %word_count) {
+  my $formatted_word_count = sprintf("%03d", $word_count{$word});
+  print $output_filehandle
+    "<a href=\"javascript:highlight('$word');\">$formatted_word_count&nbsp;&nbsp;$word</a>\n"
+}
+
+close $output_filehandle;
